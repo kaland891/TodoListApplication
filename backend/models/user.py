f2288f0aa6439a1966c000f5f97225f993ab42d1
HaiwenZhang <haiwensummer@gmail.com>
2023-05-31 20:55:53 +0800

    Fix db migration bug and add user_id in todo table

--- backend/models/user.py
+++ backend/models/user.py
@@ -7,9 +7,9 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    name = Column(String(200), nullable=False)
+    email = Column(String(200), unique=True, index=True, nullable=False)
+    hashed_password = Column(String(200), nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, default=datetime.utcnow)
     updated_at = Column(TIMESTAMP(timezone=True), nullable=False,
