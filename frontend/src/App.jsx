e24122b4887a24cf2de74902e2fa43b36fc3f6a9
zcxxxxxxx <2049314799@qq.com>
2023-06-10 21:38:03 +0800

    完成第一个组件

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
+import Heading from "./components/Heading";
+
 function App() {
   return (
     <div className="App font-Poppins container py-16 px-6 min-h-screen mx-auto">
-      App
+      <Heading />
     </div>
   );
 }
