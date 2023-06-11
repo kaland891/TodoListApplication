213dd53a7c66e221a520c4369420a407fd8a26ab
Mright-Red <3112069360@qq.com>
2023-06-12 04:18:59 +0800

    backend-step2 finished

--- backend/api/todos.py
+++ backend/api/todos.py
@@ -11,3 +11,20 @@ def get_all_todos():
 @router.post("/")
 def create_todo():
     return {"post": "todos"}
+
+
+@router.put("/{todo_id}")
+def update_todo():
+    todo_id: int
+    todo_id = 1  # 根据实际需要赋予一个值
+
+    return {"todo_id": todo_id}
+
+
+@router.delete("/{todo_id}")
+def delete_todo():
+    todo_id: int
+    q: str = None
+    todo_id = 2  # 根据实际需要赋予一个值
+
+    return {"todo_id": todo_id, "q": q}
--- backend/api/users.py
+++ backend/api/users.py
@@ -2,7 +2,40 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+RECIPES = [
+    {
+        "id": 1,
+        "title": "Apple Pie",
+        "ingredients": ["apple", "pie"],
+        "instructions": "Boil apples",
+    },
+    {
+        "id": 2,
+        "title": "Apple Pie",
+        "ingredients": ["apple", "salad"],
+        "instructions": "Raw apples",
+    },
+]
+
 
 @router.post("/")
 def create_user():
     return {"post": "users"}
+
+
+@router.get("/test/{id}", status_code=200)
+def update_user(*, id: int) -> dict:
+    # print(type(id))  # ADDED
+    result = [recipe for recipe in RECIPES if recipe["id"] == id]
+    if result:
+        return result[0]
+
+
+@router.put("/name")
+def update_user():
+    return {"name": "tom"}
+
+
+@router.put("/password")
+def update_user():
+    return {"password": 123}
