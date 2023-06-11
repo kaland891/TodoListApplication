de0ed0af61e4fcbb4bf0adc8c16beae8c3441448
zcxxxxxxx <2049314799@qq.com>
2023-06-12 01:14:36 +0800

    增加Slice

--- frontend/src/Store/store.jsx
+++ frontend/src/Store/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import TodoReducer from "../ToDoSlice";
+import UserReducer from "../UserSlice";
 
 const store = configureStore({
   reducer: {
     todo: TodoReducer,
+    user: UserReducer,
   },
 });
 
