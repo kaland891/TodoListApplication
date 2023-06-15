67861ed41611464eac4511ccb28d60f4f7ce87f7
Mright-Red <3112069360@qq.com>
2023-06-16 02:30:24 +0800

    gengxin

--- backend/main.py
+++ backend/main.py
@@ -21,4 +21,4 @@ async def root():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", reload=True, host="localhost", port=8000)
